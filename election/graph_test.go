858144e7fe315ab8d790cc96f393c122e61e3940
Chad Ostler <ostler.c@gmail.com>
2014-10-26 14:44:37 -0600

    Add unit tests for graph conversion

--- election/graph_test.go
+++ election/graph_test.go
@@ -2,10 +2,12 @@ package election
 
 import (
 	"encoding/json"
+	"strings"
 	"testing"
 )
 
-const testGraph = `{
+const (
+	testGraph = `{
 	"nodes": {
 		"2": {
 			"edges": {
@@ -20,6 +22,39 @@ const testGraph = `{
 		}
 	}
 }`
+)
+
+var (
+	d1 = []string{`digraph G {`, `1 -> 2 [label="3"];`, `1 -> 0 [label="3"];`, `2 -> 0 [label="3"];`}
+	j1 = []string{`{"nodes":{`, `"0":{}`, `"1":{"edges":{`, `"0":3`, `"2":3`, `"2":{"edges":{"0":3}`}
+)
+
+func has(str string, l []string) bool {
+	for _, s := range l {
+		if strings.Index(str, s) == -1 {
+			return false
+		}
+	}
+
+	return true
+}
+
+func TestGraphEdgesDot(t *testing.T) {
+	e := &Election{V: []*Vote{
+		&Vote{C: map[string]int{"0": 1, "1": 2, "2": 0}, W: 1},
+		&Vote{C: map[string]int{"0": 1, "1": 2, "2": 0}, W: 1},
+		&Vote{C: map[string]int{"0": 1, "1": 2, "2": 0}, W: 1}},
+		N: 3,
+	}
+	d := e.Graph().Edges().Dot()
+	if !has(d, d1) {
+		t.Fatal("Invalid dot graph generated", d)
+	}
+	j := e.Graph().Edges().JSON()
+	if !has(j, j1) {
+		t.Fatal("Invalid json", j)
+	}
+}
 
 func TestSlater(t *testing.T) {
 	j := &jsonegraph{}
